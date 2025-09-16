4653e16ff9633f8796c2137644223b8f407aea65
macrobian88 <ramshankarsc9@gmail.com>
2025-09-16 16:28:35 +0530

    Add hooks configuration

---
+++ wix_integration/hooks.py
@@ -0,0 +1,22 @@
+from . import __version__ as app_version
+
+app_name = "wix_integration"
+app_title = "Wix Integration"
+app_publisher = "Custom"
+app_description = "Integrate Frappe Items with Wix Products"
+app_email = "admin@example.com"
+app_license = "MIT"
+
+# Document Events
+doc_events = {
+    "Item": {
+        "after_insert": "wix_integration.wix_integration.utils.item_hooks.create_wix_product",
+        "on_update": "wix_integration.wix_integration.utils.item_hooks.update_wix_product"
+    }
+}
+
+# Required apps
+required_apps = ["frappe"]
+
+# Installation
+after_install = "wix_integration.install.after_install"
