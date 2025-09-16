d04f0e64720b5bc4613fd24abc2514e807e338ef
macrobian88 <ramshankarsc9@gmail.com>
2025-09-16 16:42:29 +0530

    Add desktop configuration for the module

---
+++ wix_integration/wix_integration/config/desktop.py
@@ -0,0 +1,13 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Wix Integration",
+            "_doctype": "",
+            "color": "grey",
+            "icon": "octicon octicon-cloud-upload",
+            "type": "module",
+            "description": "Integrate Frappe Items with Wix Products"
+        }
+    ]
